14d80860a8bba01d09d8f3731f05e54b0f494f3d
Zhi Ping <jenzp21@gmail.com>
2025-03-07 12:37:36 +0800

    Added XSS filter.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
+ 
+// Hackers can exploit known vulnerabilities in Express/Node if they see that your site is powered by Express. 
+// X-Powered-By: Express is sent in every request coming from Express by default. 
 app.use(helmet.hidePoweredBy()); 
+
+// Prevent Clickjacking that tricks users into interacting with a page different from what the user thinks it is. 
+// This can be obtained by executing your page in a malicious context, by means of iframing. 
 app.use(helmet.frameguard({ action: 'deny' }));
+
+// Cross-site scripting (XSS) injects malicious scripts into vulnerable pages to steal sensitive data.
+// With this, the browser detects a potential injected script using a heuristic filter, changes and neitralizes the script code.
+app.use(helmet.xssFilter());
 
 
 
